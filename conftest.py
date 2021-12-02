a461ebe6c25c48f8bec9cb28a13f6bfa432bc646
BasGit0212 <forusing@mail.ru>
2021-12-02 20:59:28 +0300

    gitignore test

--- conftest.py
+++ conftest.py
@@ -11,7 +11,7 @@ def pytest_addoption(parser):
 @pytest.fixture(scope='function')
 def browser(request):
     language = request.config.getoption('language')
-    print('\nInit language on', language)
+    print('\nInit language on ...', language)
 
     browser_name = request.config.getoption('browser_name')
     browser = None
