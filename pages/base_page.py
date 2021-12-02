ad1f1d1f36364aba385393995cd8cc97d4599e55
BasGit0212 <forusing@mail.ru>
2021-12-02 22:01:42 +0300

    Первый тест на основе Page Object (https://stepik.org/lesson/238819/step/4?auth=login&unit=211271)

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,11 @@
-
+from selenium.webdriver import Remote as RemoteWebDriver
 
 
 class BasePage():
-    def __init__(self, browser, url):
+    def __init__(self, browser: RemoteWebDriver, url):
         self.browser = browser
         self.url = url
 
     def open(self):
         self.browser.get(self.url)
+        
