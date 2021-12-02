ce0fe09630ae720093e1a7727ab64f1620e78fe3
BasGit0212 <forusing@mail.ru>
2021-12-02 23:00:14 +0300

    Переходы между страницами/. Вариант 2 (https://stepik.org/lesson/238819/step/9?auth=login&unit=211271)

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,5 +1,6 @@
 from pages.base_page import BasePage
-from pages.login_page import LoginPage
+# Вариант 1
+# from pages.login_page import LoginPage
 from pages.locators import MainPageLocators
 from selenium.webdriver.common.by import By
 
@@ -11,4 +12,5 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
-        return LoginPage(browser=self.browser, url=self.browser.current_url)
+        # Вариант 1
+        # return LoginPage(browser=self.browser, url=self.browser.current_url)
--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,8 @@
 # pytest -v -s --tb=line --language=en test_main_page.py
 
 from pages.main_page import MainPage
+# Вариант 2
+from pages.login_page import LoginPage
 
 
 def test_guest_should_see_login_link(browser):
@@ -14,5 +16,11 @@ def test_guest_can_go_to_login_page(browser):
     link = 'http://selenium1py.pythonanywhere.com'
     page = MainPage(browser, link)
     page.open()
-    login_page = page.go_to_login_page()
+    # Вариант 1
+    # login_page = page.go_to_login_page()
+    # login_page.should_be_login_page()
+    # Вариант 2
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
+    
