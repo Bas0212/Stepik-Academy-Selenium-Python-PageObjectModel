b721019f05773734c2027babcde547718a1486e7
BasGit0212 <forusing@mail.ru>
2021-12-02 22:56:50 +0300

    Переходы между страницами. Вариант 1 (https://stepik.org/lesson/238819/step/9?auth=login&unit=211271)

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,4 +1,5 @@
 from pages.base_page import BasePage
+from pages.login_page import LoginPage
 from pages.locators import MainPageLocators
 from selenium.webdriver.common.by import By
 
@@ -10,3 +11,4 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
+        return LoginPage(browser=self.browser, url=self.browser.current_url)
